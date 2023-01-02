c4913228b6791ea5be30847bb9847396491e030a
master <master.code.develop@gmail.com>
2023-01-02 15:42:13 +0100

    update styles header

--- app/src/layouts/Header.jsx
+++ app/src/layouts/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import logo from '../assets/img/logo.png';
 
 export default function Header() {
   return (
     <header className="header">
       <Link className="header-logo" to="/">
-        LOGO
+        <img src={logo} alt="Wealth Health Logo" />
       </Link>
       <nav className="header-nav">
-        <NavLink to="/employees">View Current Employees</NavLink>
+        <NavLink className="btn btn-secondary" to="/employees">
+          View Current Employees
+        </NavLink>
       </nav>
     </header>
   );
