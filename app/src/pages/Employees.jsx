db4d56ffcac120c4abf2092df7fe5f7efbe1c802
master <master.code.develop@gmail.com>
2023-01-04 18:43:49 +0100

    add search table

--- app/src/pages/Employees.jsx
+++ app/src/pages/Employees.jsx
@@ -6,7 +6,21 @@ export default function Employees() {
   const { list } = useSelector((state) => state.employee);
   const [pageSize, setPageSize] = useState(5);
   const onChangePageSize = (e) => setPageSize(e.target.value);
-
+  const [row, setRow] = useState(list);
+  const onSearch = ({ target }) => {
+    let data = [];
+    list.map((e) => {
+      const object = Object.keys(e);
+      for (let i = 0; i < object.length; i++) {
+        const element = e[object[i]];
+        if (element.toLowerCase().includes(target.value.toLowerCase())) {
+          data.push(e);
+          break;
+        }
+      }
+    });
+    setRow(data);
+  };
   return (
     <main className="employees">
       <div className="employees-header">
@@ -22,13 +36,13 @@ export default function Employees() {
             </select>
             <span>entries</span>
           </div>
-          <div className="search">
+          <div className="search" onChange={onSearch}>
             <label htmlFor="search">Search:</label>
             <input type="search" name="search" id="search" />
           </div>
         </div>
       </div>
-      {list ? <CustomTable data={list} pageSize={pageSize} /> : <></>}
+      {list ? <CustomTable data={row} pageSize={pageSize} /> : <></>}
     </main>
   );
 }
